5185e7afa79307b3a219fa5b069b454c27094818
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-07-24 22:51:40 +0100

    Syncing with codecrafters-io/languages
    Created by https://github.com/codecrafters-io/languages

--- app/main.cr
+++ app/main.cr
@@ -1,11 +1,14 @@
 require "socket"
 
+# Ensure that the program terminates on SIGTERM, https://github.com/crystal-lang/crystal/issues/8687
+Signal::TERM.trap { exit }
+
 class YourRedisServer
   def start
     puts("Your code goes here!")
 
     # Uncomment this block to pass the first stage
-    # server = TCPServer.new("localhost", 6379, reuse_port: true)
+    # server = TCPServer.new("0.0.0.0", 6379, reuse_port: true)
     # client = server.accept?
   end
 end
